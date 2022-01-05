27822706fd1d314658726d6abf6c2220d511c131
wafu-alt <rhfemtlfem@naver.com>
2022-01-05 22:02:54 +0900

    Update main.js
    왜 안되는것이야

--- dreamCoding/mini-shop/main.js
+++ dreamCoding/mini-shop/main.js
@@ -10,13 +10,14 @@ function loadItems() {
 fetch("data/data.json")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
 //main
 // json을 동적으로 읽어와서 item 전달
 // 프로미스가 리턴이 되면 > html에 아이템을 보여주고, 버튼을 클릭했을때 필터링 되게함
 
-// loadItems()
-//   .then((items) => {
-//     // displayItems(items);
-//     // setEventListeners(items);
-//   })
-//   .catch(console.log);
+loadItems()
+  .then((items) => {
+    // displayItems(items);
+    // setEventListeners(items);
+  })
+  .catch(console.log);
